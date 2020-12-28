51b4135644d4e8c00a14ba13acf04c548883874a
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2020-12-28 16:06:24 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -13,9 +13,10 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
 
+sed -i 's/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
+
 sed -i 's/ucidef_set_interface_lan 'eth0'/ucidef_set_interface_wan 'eth0'/g' package/base-files/files/etc/board.d/99-default_network
 sed -i 's/ucidef_set_interface_wan 'eth1'/ucidef_set_interface_lan 'eth1'/g' package/base-files/files/etc/board.d/99-default_network
-
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
