1e0dc7f982532d7cdbd9e72c640ba3f8dc67133f
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2020-12-29 11:34:28 +0800

    Update 1.sh

--- hc5962/1.sh
+++ hc5962/1.sh
@@ -14,3 +14,6 @@
 sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
 #
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+#
+cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
+cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
