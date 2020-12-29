c205c63f3ef4477ff811f00d335df3dc003584fd
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2020-12-29 13:39:56 +0800

    Update 2.sh

--- hc5962/2.sh
+++ hc5962/2.sh
@@ -11,4 +11,9 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
+#
+sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+#
+cp -r /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
+cp -r /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
