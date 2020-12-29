a58f020dccbcbc7eb49e5c9b38e9b146ad662773
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2020-12-29 10:51:19 +0800

    Update 1.sh

--- hc5962/1.sh
+++ hc5962/1.sh
@@ -11,9 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
-
+#
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
-
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
