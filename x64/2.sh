15ae730c8dffb5e5b525b210c1a22c4202e545dc
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2021-01-01 12:59:14 +0800

    Create 2.sh

---
+++ x64/2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+#
+cp -r /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
+cp -r /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
+#
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/wireless.radio${devidx}.disabled/a\			set wireless.radio${devidx}.cell_density=0' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/encryption=none/encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/wireless.default_radio${devidx}.encryption/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
