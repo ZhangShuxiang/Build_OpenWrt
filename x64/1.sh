83891d359fcbc5cf87427d6c5884f3c723021b1b
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2021-01-01 12:56:52 +0800

    Create 1.sh

---
+++ x64/1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
+#
+#sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages/tree/packages-19.07' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
