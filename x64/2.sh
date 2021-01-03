47365103aa648acbe752f9b0e8a56acca6c6f80c
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2021-01-03 10:59:17 +0800

    Update 2.sh

--- x64/2.sh
+++ x64/2.sh
@@ -12,14 +12,19 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.20/g' package/base-files/files/bin/config_generate
+sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
+sed -i "/system.@system[-1].timezone/a\		set system.@system[-1].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
+sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/bin/config_generate
+sed -i "s/'1.openwrt.pool.ntp.org'/'cn.ntp.org.cn'/g" package/base-files/files/bin/config_generate
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 #
 cp -r /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
 cp -r /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
-cp /workdir/openwrt/build_dir/target-x86_64_musl/linux-x86_64/*/drivers/thermal/thermal_core.c thermal_sys.ko
 #
+sed -i 's/htmode=HT20/htmode=HT40/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/wireless.radio${devidx}.disabled/a\			set wireless.radio${devidx}.cell_density=0' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/wireless.radio${devidx}.disabled/a\			set wireless.radio${devidx}.noscan=1' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/encryption=none/encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/wireless.default_radio${devidx}.encryption/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
